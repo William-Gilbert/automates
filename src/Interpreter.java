4ee43ed6d7d7181cc35376acaa814fb46508669b
coren_000 <corentin25@gmail.com>
2016-02-09 17:24:13 +0100

    recherche de l'erreur pour identifier les doublons

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -11,12 +11,20 @@ public class Interpreter implements Visiteur{
     public Object visit(Automate a) {
         for (Etat e : a.listEtat){
             for(Etat e2 : a.listEtat){
-                if(e!=e2 && ((String)visit(e)).equals(((String)visit(e2)))){//Cast peut-être inutile
+                //affiche le déroulement des actions
+                //lorsqu'il voit true true il devrait retourner false mais il ne le fait pas
+                //et il ne parcours pas tout à chaque fois, il fait une vérification au lieu de de 3 par etat !
+                String s1=(String)visit(e);
+                String s2=(String)visit(e2);
+                System.out.println(s1+" "+s2);
+                System.out.print(e!=e2);
+                System.out.println(s1.equals(s2));
+                if(e!=e2 && s1.equals(s2)){
                     return false;
                 }
             }
             // Traiteemnt type d'objet
-            //for(Automate sa : e.sousAutomates){if(!visit(sa))return false;}
+            for(Automate sa : e.sousAutomates){if(visit(sa)==null)return false;}
             visit(e);
         }
         return true;
